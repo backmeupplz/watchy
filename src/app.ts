62bbd5fb30df5518cd53489bd8b9bd8270e3a3ff
backmeupplz <backmeupplz@gmail.com>
2018-12-19 21:33:53 -0800

    add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.info('Ну привет-привет!')
+// Config dotenv
+import * as dotenv from 'dotenv'
+dotenv.config({ path: `${__dirname}/../.env` })
+
+// Imports
+import { bot } from './helpers/bot'
+
+bot.on('message', ctx => {
+  ctx.reply('Ну привет-привет!')
+})
---
+++ src/helpers/bot.ts
@@ -0,0 +1,12 @@
+// Imports
+import { ContextMessageUpdate, Telegraf } from 'telegraf'
+const TelegrafBot = require('telegraf')
+
+export const bot = new TelegrafBot(process.env.TOKEN) as Telegraf<
+  ContextMessageUpdate
+>
+
+bot.telegram.getMe().then(botInfo => {
+  const anybot = bot as any
+  anybot.options.username = botInfo.username
+})
