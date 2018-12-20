bc1eb26c3aeded5010d94d078aab08d29d29d142
backmeupplz <backmeupplz@gmail.com>
2018-12-19 23:57:09 -0800

    add valid error

--- src/helpers/error.ts
+++ src/helpers/error.ts
@@ -2,6 +2,7 @@ export function checkIfErrorDismissable(error: Error) {
   const dismissableMessages = [
     "message can't be deleted",
     'bot was kicked from the supergroup',
+    'group chat was upgraded to a supergroup',
   ]
   for (const message of dismissableMessages) {
     if (error.message.indexOf(message) > -1) {
